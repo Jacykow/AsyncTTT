50cbe1fa1a668cb4d4fc42ae00f5780a520e10af
Mezzzer <36813308+Mezzzer@users.noreply.github.com>
2020-01-12 16:06:46 +0100

    kolejny commit wip

--- AsyncTTT Backend/Controllers/FriendsController.cs
+++ AsyncTTT Backend/Controllers/FriendsController.cs
@@ -1,4 +1,5 @@
 ﻿using AsyncTTT_Backend.Models;
+using AsyncTTT_Backend.Models.Internal;
 using AsyncTTT_Backend.SQL;
 using AsyncTTT_Backend.Utility;
 using Microsoft.AspNetCore.Mvc;
@@ -10,31 +11,22 @@ namespace AsyncTTT_Backend.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UserController : ControllerBase
+    public class FriendsController : ControllerBase
     {
-        [HttpGet]
-        public DefaultResponse Get()
+
+        [HttpGet(Name = "GetFriends")]
+        public IEnumerable<User> GetFirends(int id)
         {
             var credentials = ControllerUtility.GetCredentials(Request.Headers);
-            return new DefaultResponse
-            {
-                Success = true,
-                Message = $"<{credentials.login}> <{credentials.password}>"
-            };
-        }
 
-        // To jest testowe i do wywalenia
-        [HttpGet("{id}", Name = "Get")]
-        public IEnumerable<User> Get(int id)
-        {
             var sqlCommand = new SimpleSqlCommand<User>()
             {
-                SqlCommand = "SELECT * FROM Players WHERE id_player > @id",
+                SqlCommand = "SELECT id_cred, id_player FROM friendships JOIN players on(player2 = id_player) WHERE player1 = ((SELECT id_player FROM credentials c join players p on(c.id_cred =  p.id_cred) WHERE nickname = @nick))",
                 Parameters = new SqlParameter[]
                 {
-                    new SqlParameter("@id", SqlDbType.Int)
+                    new SqlParameter("@nick", SqlDbType.VarChar)
                     {
-                        Value = id
+                        Value = credentials.login
                     }
                 },
                 ModelExtractor = reader => new User
@@ -49,7 +41,7 @@ namespace AsyncTTT_Backend.Controllers
 
 
         [HttpPost]
-        public void Post([FromBody] User value)
+        public void Post([FromBody] Invitation value)
         {
 
         }
---
+++ AsyncTTT Backend/Models/Move.cs
@@ -0,0 +1,9 @@
+﻿namespace AsyncTTT_Backend.Models
+{
+    public class Move
+    {
+        public int IdGame { get; set; }
+        public int XCoord { get; set; }
+        public int YCoord { get; set; }
+    }
+}
