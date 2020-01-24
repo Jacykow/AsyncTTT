82707cc0e421f9e94aeae2183b1341d85945cd70
Jacykow <jacgulij@gmail.com>
2020-01-24 23:30:22 +0100

    wip

---
+++ AsyncTTT/Assets/Scripts/Managers/PopupManager.cs
@@ -0,0 +1,47 @@
+﻿using TMPro;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Managers
+{
+    public class PopupManager : MonoBehaviour
+    {
+        private static PopupManager _manager;
+
+        public static PopupManager Main
+        {
+            get
+            {
+                if (_manager == null)
+                {
+                    Debug.LogError("No popup manager instance detected!");
+                }
+                return _manager;
+            }
+        }
+
+        [SerializeField]
+        private Button _okButton;
+        [SerializeField]
+        private TextMeshProUGUI _popupText;
+        [SerializeField]
+        private GameObject _popupContainer;
+
+        private void Awake()
+        {
+            _manager = this;
+            _popupContainer.SetActive(false);
+            _okButton.OnClickAsObservable().Subscribe(_ =>
+            {
+                _popupContainer.SetActive(false);
+            }).AddTo(this);
+        }
+
+        public void ShowPopup(string message)
+        {
+            _popupContainer.SetActive(true);
+            _popupText.text = message;
+        }
+    }
+}
--- AsyncTTT/Assets/Scripts/ViewModels/MainViewModel.cs
+++ AsyncTTT/Assets/Scripts/ViewModels/MainViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using Assets.Scripts.Abstraction;
+using Assets.Scripts.BLL.Operations;
 using Assets.Scripts.Managers;
+using TMPro;
 using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,7 +11,9 @@ namespace Assets.Scripts.ViewModels
     public class MainViewModel : ViewModel
     {
         [SerializeField]
-        private Button _friends, _games;
+        private Button _friends, _games, _login, _register;
+        [SerializeField]
+        private TMP_InputField _loginInput, _passwordInput;
 
         private void Start()
         {
@@ -22,6 +26,82 @@ namespace Assets.Scripts.ViewModels
             {
                 ViewManager.Main.ChangeView("Games");
             }).AddTo(this);
+
+            _login.OnClickAsObservable().SelectMany(_ =>
+            {
+                ValidateCredentials();
+                AuthorizationManager.Main.Login = _loginInput.text;
+                AuthorizationManager.Main.Password = _passwordInput.text;
+                return new CheckLogin().Execute();
+            }).Subscribe(_ =>
+            {
+
+            }, error =>
+            {
+                PopupManager.Main.ShowPopup(error.Message);
+            }).AddTo(this);
+
+            _register.OnClickAsObservable().SelectMany(_ =>
+            {
+                ValidateCredentials();
+                return new Register().Execute();
+            }).Subscribe(_ =>
+            {
+
+            }, error =>
+            {
+                PopupManager.Main.ShowPopup(error.Message);
+            }).AddTo(this);
+        }
+
+        private bool ValidateCredentials()
+        {
+            string validateMessage;
+            validateMessage = IsValidLogin(_loginInput.text);
+            if (validateMessage != null)
+            {
+                PopupManager.Main.ShowPopup(validateMessage);
+                return false;
+            }
+            validateMessage = IsValidPassword(_passwordInput.text);
+            if (validateMessage != null)
+            {
+                PopupManager.Main.ShowPopup(validateMessage);
+                return false;
+            }
+            return true;
+        }
+
+        private string IsValidLogin(string login)
+        {
+            if (login.Length < 6)
+            {
+                return "Za krótki login!";
+            }
+            foreach (char c in login)
+            {
+                if (char.IsLetterOrDigit(c) == false)
+                {
+                    return "Niedozwolone znaki w loginie!";
+                }
+            }
+            return null;
+        }
+
+        private string IsValidPassword(string password)
+        {
+            if (password.Length < 6)
+            {
+                return "Za krótkie hasło!";
+            }
+            foreach (char c in password)
+            {
+                if (char.IsLetterOrDigit(c) == false)
+                {
+                    return "Niedozwolone znaki w haśle!";
+                }
+            }
+            return null;
         }
     }
 }
