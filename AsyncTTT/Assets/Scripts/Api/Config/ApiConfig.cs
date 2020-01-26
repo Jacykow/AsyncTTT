8999afc8464b6a7d6133af7d724f7adafea2adfd
Jacykow <jacgulij@gmail.com>
2020-01-26 20:47:58 +0100

    added get games

--- AsyncTTT/Assets/Scripts/Api/Config/ApiConfig.cs
+++ AsyncTTT/Assets/Scripts/Api/Config/ApiConfig.cs
@@ -8,6 +8,8 @@
             public static string AzureUser => AZURE + "/user";
             public static string AzureCredentials => AZURE + "/credentials";
             public static string AzureBoard => AZURE + "/board";
+            public static string AzureGame => AZURE + "/game";
+            public static string AzureGameHistory => AZURE + "/gamehistory";
             public static string AzureBoardId => AZURE + "/boardid";
             public static string AzureFriends => AZURE + "/friends";
             public static string AzureFriendsInvitation => AZURE + "/friendsinvitation";
