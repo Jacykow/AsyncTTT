9e9f3176c06fc9c45bc0147850373504114e8436
Jacykow <jacgulij@gmail.com>
2020-01-26 22:37:02 +0100

    wip

--- AsyncTTT/Assets/Scripts/BLL/Operations/GetBoard.cs
+++ AsyncTTT/Assets/Scripts/BLL/Operations/GetBoard.cs
@@ -22,16 +22,24 @@ namespace Assets.Scripts.BLL.Operations
 
         public IObservable<Game> Execute()
         {
-            return new AzureApiQuery<List<Move>>(ApiConfig.Endpoints.AzureBoardId, HttpMethod.Get)
+            return new AzureApiQuery<List<Move>>(ApiConfig.Endpoints.AzureBoardId + $"/{_game.Id}", HttpMethod.Get)
                 .Execute().Select(moves =>
                 {
                     _game.Board = new int[20, 20];
                     int playerId = AuthorizationManager.Main.Id;
+
+                    for (int y = 0; y < 20; y++)
+                    {
+                        for (int x = 0; x < 20; x++)
+                        {
+                            _game.Board[x, y] = -1;
+                        }
+                    }
                     foreach (var move in moves)
                     {
                         _game.Board[move.XCoord, move.YCoord] =
                             move.id_player == 0 ? -1 :
-                            move.id_player == playerId ? 0 : 1;
+                            move.id_player == playerId ? 0 : -1;
                     }
                     return _game;
                 });
